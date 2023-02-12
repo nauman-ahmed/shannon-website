93c860be187209e82dc7651c48b5cec2ee05f732
danish <danishgoheer17@gmail.com>
2023-02-12 18:30:28 +0500

    2/12/2023 danishh

--- src/containers/clientSite/divisions.js
+++ src/containers/clientSite/divisions.js
@@ -179,11 +179,12 @@ const images = window.location.origin + "/assets/images";
                                       className="w-100 h-100  "
                                     // style={{ width: "100%", height: "100%" }}
                                     />
-                                  <div className="artistnametext-v3">
+                                  <p className="card_img_text2 pt-2">
                                   
                                         {item1.artistId.lastname}{" "}
                                         {item1.artistId.firstname}
-                                  </p>    
+                                     </p>
+                                     </div>
                                   </Link>
                                 </>
                               ) : (
