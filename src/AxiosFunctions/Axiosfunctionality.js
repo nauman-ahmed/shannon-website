cb278cb7328224f0cc6a36de3a7760b9194d0ad1
Nauman Ahmed <naumanahmed449@gmail.com>
2023-03-28 19:01:45 +0200

    changed axios

--- src/AxiosFunctions/Axiosfunctionality.js
+++ src/AxiosFunctions/Axiosfunctionality.js
@@ -8,8 +8,8 @@ import axios from "axios";
 // export const IMAGE_ROUTE = "http://localhost:5001/";
 
 
-export const BASE_URL = "http://3.132.94.46:5000/api/"
-export const IMAGE_ROUTE = "http://3.132.94.46:5000/";
+export const BASE_URL = "http://18.224.52.5:5000/api/"
+export const IMAGE_ROUTE = "http://18.224.52.5:5000/";
 
 
 export const logouter = () => {
