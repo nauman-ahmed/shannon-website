b70a4cd5a914b41d691e4d2821d9d8d9b7f3f5ec
Nauman Ahmed <35535536+nauman-ahmed@users.noreply.github.com>
2023-04-10 20:15:32 +0200

    updated font weight

--- src/components/layout/divisionSideBar.js
+++ src/components/layout/divisionSideBar.js
@@ -61,7 +61,7 @@ function DivisionSideBar(props) {
 
   return (
     <div id="w-node-_783b3b4a-9daa-ecbf-356e-d84dbb36f4cc-bb36f4cc" className="respdivhide">
-      <h3 className="homeh3" style={{ color:"#000000", fontFamily: "Montserrat, sans-serif", textUnderlineOffset : "5px" }}>{
+      <h3 className="homeh3" style={{ color:"#000000", fontFamily: "Montserrat, sans-serif", textUnderlineOffset : "5px", fontWeight: 800 }}>{
         props.activeBtn === "illustration-artists" ? "ILLUSTRATION" 
         : props.activeBtn === "cgi" ? "CGI" 
         : props.activeBtn === "photography" ? "PHOTOGRAPHY" 
