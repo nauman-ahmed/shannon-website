85cadebc583ee203bce8a041ae495641fd78ae74
JOSE <marketing@iMac-de-JOSE.local>
2023-09-12 10:49:38 -0600

    UPDATE - Admin site, now the IMAGES SUBMISSIONS can be deleted

--- src/containers/admin/artistsPages/portfolio.js
+++ src/containers/admin/artistsPages/portfolio.js
@@ -397,6 +397,19 @@ function Portfolio(props) {
             {Object.keys(props.selectedImages).length > 0 ? props.selectedImages.mainImage.map((item,key)=>
                 (item.statusSubmit === 1 && item.status === 0?
                   <div key={key} onClick={()=>props.history.push({pathname:"/admin/artists/"+item._id,state: { selectedArtist: props.selectedArtist,selectedImages:props.selectedImages }})} className='artistcardAdminPortfolio w-inline-block'>
+                    <div className="crossSection">
+                      <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="12px"
+                          height="12px"
+                          viewBox="0 0 352 512"
+                          onClick={() => deleteImageHandler(item)}>
+                      <path
+                      fill="grey"
+                      d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+                      />
+                      </svg>
+                    </div>
                     <img alt='' src={item.path} className="image" style={{cursor:"pointer"}} />
                   </div>
                 :null)
