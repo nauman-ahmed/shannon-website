fa2730ae791f01f293146287f0a2992531bccd16
JOSE <marketing@iMac-de-JOSE.local>
2023-09-19 12:57:14 -0600

    FIX - Division page -  artist fitler

--- src/containers/clientSite/divisions.js
+++ src/containers/clientSite/divisions.js
@@ -241,7 +241,7 @@ const images = window.location.origin + "/assets/images";
                         className="detail_card3"
                         // style={{ paddingTop: "10px" }}
                       >
-                        {item.ImageData.map((item1, key1) => (
+                        {item.ImageData.map((item1, key1) => item1.artistId.status!=0?(
                           <>
                             <Link
                                 key={key1}
@@ -274,7 +274,7 @@ const images = window.location.origin + "/assets/images";
                                   {/* </div> */}
                               </Link>
                           </>
-                        ))}
+                        ):null)}
                       </div>
                       <div className="divisionbuttoncontainer" style={{ justifyContent: "flex-end" }}>
                         <Link
