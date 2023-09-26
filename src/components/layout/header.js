103061c0bc40cae10dd68f51ef292e2458f93a11
JOSE <marketing@iMac-de-JOSE.local>
2023-09-26 10:42:38 -0600

    UPDATE - Kidshannon menu link implemented

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -76,7 +76,7 @@ function Header(props) {
             <nav role="navigation" className="navmenu w-nav-menu">
               <span>
               <a 
-                href="http://3.132.94.46/#/" 
+                href="http://13.59.180.10/#/" 
                 target="_blank" 
                 className="navlink v2 w-nav-link ">KIDSHANNON</a>
 
