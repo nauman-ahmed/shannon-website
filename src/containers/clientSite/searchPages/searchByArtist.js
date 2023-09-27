b164e791685927a88b5d9f9a3be6fcc11953ebb7
JOSE <marketing@iMac-de-JOSE.local>
2023-09-27 10:12:45 -0600

    UPDATE - Artist portfolio change title distribution

--- src/containers/clientSite/searchPages/searchByArtist.js
+++ src/containers/clientSite/searchPages/searchByArtist.js
@@ -332,7 +332,7 @@ function SearchByArtist(props) {
 
               <div className={windowSize.innerWidth < 479 ? "" : "d-flex"} style={windowSize.innerWidth < 479 ? { marginLeft: "8%" } : { justifyContent: "space-between", marginTop: "-10px" ,marginBottom:"10px", width:"98.4%" }} > 
                 <h2 className="h2talent">{data1[search].title}</h2>  
-                <a href={"http://13.59.180.10/#/artists/"+data1[search].id} target="_blank" className="linkToKS">View Kid's portfolio</a> 
+                <a href={"http://13.59.180.10/#/artists/"+data1[search].id} target="_blank" className="linkToKS">Visit Kid's portfolio</a> 
               </div>
 
                 {windowSize.innerWidth < 479 ?
