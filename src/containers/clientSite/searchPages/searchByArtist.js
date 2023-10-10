6b9d4a56935b04e6f752c5e6699d24b0102eb452
Nauman Ahmed <35535536+nauman-ahmed@users.noreply.github.com>
2023-10-10 05:12:06 +0200

    missed one file to update size of loader

--- src/containers/clientSite/searchPages/searchByArtist.js
+++ src/containers/clientSite/searchPages/searchByArtist.js
@@ -298,7 +298,6 @@ function SearchByArtist(props) {
               className="mb-3"
               alt="loading"
               src={loading}
-              style={{ width: "50px" }}
             />
           </div>
       )
