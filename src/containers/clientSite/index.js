65d9c03883160f200a1190cb38578502eb0aa6bc
JOSE <marketing@iMac-de-JOSE.local>
2023-11-15 09:37:06 -0600

    FIX- Categories current style fixed.

--- src/containers/clientSite/index.js
+++ src/containers/clientSite/index.js
@@ -93,7 +93,7 @@ function Index(props) {
         dispatch(ArtistDataAPI());
         dispatch(keywordDataApi());
         dispatch(RecentlyArtistImageSliceData());
-        localStorage.setItem("routePaths","");
+        localStorage.clear();
     }, [])
 
     useEffect(() => {
