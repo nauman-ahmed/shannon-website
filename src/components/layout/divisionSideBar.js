8bc18786230f8d65749fec02ef18a11f0d7d82bb
JOSE <marketing@iMac-de-JOSE.local>
2023-11-21 10:30:28 -0600

    UPDATE - Categories 'Select by artist' sidemenu text change to fit the category the user choose.

--- src/components/layout/divisionSideBar.js
+++ src/components/layout/divisionSideBar.js
@@ -156,7 +156,7 @@ function DivisionSideBar(props) {
               
               </div>
           )):""} */}
-          <h3 className="homeh3" style={{textDecorationLine:"none"}}>SELECT BY ARTIST</h3>
+          <h3 className="homeh3" style={{textDecorationLine:"none"}}> { localStorage.getItem("Category") !== "none" && !pagesWithSideMenu.includes(localStorage.getItem("Category")) ? localStorage.getItem("Category").toUpperCase()+" ARTISTS" :"SELECT BY ARTIST"}</h3>
             {alpha.map((item,key)=>
           (
             <div key={key}>
